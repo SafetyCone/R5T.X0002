75fca6413d29478b6b1b435959d3a732a586d33d
davidcoats <david@getsafetycone.com>
2022-01-04 10:57:08 -0800

    Initial commit of source-stub.

---
+++ source/R5T.X0002.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.X0002.Construction
+{
+    /// <summary>
+    /// Construction project for Add project reference and all dependencies to project and enclosing solutions. (Virconium functionality).
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.X0002/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.X0002
+{
+    /// <summary>
+    /// Add project reference and all dependencies to project and enclosing solutions. (Virconium functionality)
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
